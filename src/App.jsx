ac15a5bf3effccccd097553039a729b3579d2f4b
omarkhalid12 <ok3817554@gmail.com>
2024-04-22 00:07:17 +0300

    Navbar With Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom'
 import AboutPage from './pages/About'
 import HomePage from './pages'
 import ProductsPage from './pages/Products'
+import NavBar from './layout/Navbar'
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/products' element={<ProductsPage />} />
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,107 @@
+'use client'
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  HStack,
+} from '@chakra-ui/react'
+import { BsMoon, BsSun } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+
+const Links = ['Dashboard', 'Products', 'Team']
+
+
+const NavLink = ({ children }) => {
+  return (
+
+    <Link
+      to={children.toLowerCase()}
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+    >
+      {children}
+    </Link>
+  )
+}
+
+const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+
+          <HStack spacing={8} alignItems={'center'}>
+          <Link to='/'>My App</Link>
+            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <BsMoon /> : <BsSun />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+
+        </Flex>
+      </Box>
+    </>
+  )
+}
+export default NavBar;
