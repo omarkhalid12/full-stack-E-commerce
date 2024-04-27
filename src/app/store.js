9dfbe7e97332ea22523ef1d28b5c9b832a882016
omarkhalid12 <ok3817554@gmail.com>
2024-04-28 02:18:02 +0300

    Shopping Cart Persist

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import LoginSlice from "./features/loginSlice";
 import cartSlice from "./features/cartSlice";
 import globalSlice from "./features/globalSlice";
+import storage from "redux-persist/lib/storage";
+import {persistReducer, persistStore} from "redux-persist";
+
+const persistCartConfig = {
+  key: "cart",
+  storage,
+}
+const persistedCart = persistReducer(persistCartConfig, cartSlice);
 
 export const store = configureStore({
   reducer: {
-    cart: cartSlice,
+    cart: persistedCart,
     login: LoginSlice,
     global: globalSlice
   }
 })
+
+export const persister = persistStore(store)
