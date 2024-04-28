ca3be99bb9f93041d10d0fb6bd00dfd1c85bd761
omarkhalid12 <ok3817554@gmail.com>
2024-04-28 03:05:05 +0300

    Display Cart Items In Cart Drawer

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -1,14 +1,19 @@
-import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Input } from "@chakra-ui/react"
+import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay } from "@chakra-ui/react"
 import { useRef } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { onCloseCartDrawerAction, selectGlobal } from "../app/features/globalSlice"
+import CartDrawerItem from "./CartDrawerItem"
+import { selectCart } from "../app/features/cartSlice"
 
 const CartDrawer = () => {
   const dispatch = useDispatch()
   const btnRef = useRef()
   const { isOpenCartDrawer } = useSelector(selectGlobal);
+  const { cartProducts } = useSelector(selectCart);
+
+
   const onClose = () => dispatch(onCloseCartDrawerAction())
-  
+
   return (
     <Drawer
         isOpen= {isOpenCartDrawer}
@@ -22,7 +27,11 @@ const CartDrawer = () => {
           <DrawerHeader>Your Shopping Cart</DrawerHeader>
 
           <DrawerBody>
-            <Input placeholder='Type here...' />
+            {
+              cartProducts.map(item => (
+                <CartDrawerItem key={item.id} {...item} />
+              ))
+            }
           </DrawerBody>
 
           <DrawerFooter>
---
+++ src/components/CartDrawerItem.jsx
@@ -0,0 +1,45 @@
+import { Button, Divider, Flex, Image, Stack, Text } from "@chakra-ui/react"
+import { useDispatch } from "react-redux"
+import PropTypes from 'prop-types';
+
+
+const CartDrawerItem = ({id, attributes: { thumbnail, title, price }, quantity}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <Flex alignItems={"center"} justifyContent={"space-between"} mb={3} py={2}>
+        <Image
+          src={`${import.meta.env.VITE_SERVER_URL}${thumbnail.data.attributes.url}`}
+          alt={title}
+          w={"80px"}
+          h={"80px"}
+          rounded="full"
+          objectFit={"cover"}
+          mr={5}
+        />
+        <Stack>
+          <Flex >
+            <Text fontSize={"sm"}>{title}</Text>
+            <Text fontSize={"sm"} ml={3}>Price: ${price}</Text>
+          </Flex>
+          
+          <Text fontSize={"sm"}>Quantity: {quantity}</Text>
+          <Button variant="solid" colorScheme="red" size="xs" w="fit-content">
+            Remove
+          </Button>
+        </Stack>
+      </Flex>
+
+      <Divider />
+    </>
+  )
+}
+
+CartDrawerItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  attributes: PropTypes.string.isRequired,
+  quantity: PropTypes.string.isRequired,
+};
+
+export default CartDrawerItem
