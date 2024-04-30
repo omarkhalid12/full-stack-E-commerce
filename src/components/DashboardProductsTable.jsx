fa436b7fb6944fc8ab5945079251e924d30ee3c5
omarkhalid12 <ok3817554@gmail.com>
2024-05-01 00:22:46 +0300

    Display Dashboard Products

--- src/components/DashboardProductsTable.jsx
+++ src/components/DashboardProductsTable.jsx
@@ -8,49 +8,87 @@ import {
   Td,
   TableCaption,
   TableContainer,
+  Image,
+  Button,
 } from '@chakra-ui/react';
 import TableSkeleton from './TableSkeleton';
 import { useGetDashboardProductsQuery } from '../app/services/apiSlice';
+import { AiOutlineEye } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { BsTrash } from 'react-icons/bs';
+import { FiEdit2 } from 'react-icons/fi';
 
 const DashboardProductsTable = () => {
 
   const {isLoading, isError, data} = useGetDashboardProductsQuery({ page: 1});
   console.log({isLoading, isError, data})
-  return <TableSkeleton />
+  if(isLoading) return <TableSkeleton />
 
   return (
-    <TableContainer>
+    <TableContainer maxW={"85%"} mx={"auto"}>
       <Table variant='simple'>
         <TableCaption>Imperial to metric conversion factors</TableCaption>
         <Thead>
           <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
+            <Th>ID</Th>
+            <Th>Title</Th>
+            <Th>Category</Th>
+            <Th>Thumbnail</Th>
+            <Th isNumeric>Price</Th>
+            <Th isNumeric>Stock</Th>
+            <Th>Action</Th>
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
-            <Td isNumeric>25.4</Td>
-          </Tr>
-          <Tr>
-            <Td>feet</Td>
-            <Td>centimetres (cm)</Td>
-            <Td isNumeric>30.48</Td>
-          </Tr>
-          <Tr>
-            <Td>yards</Td>
-            <Td>metres (m)</Td>
-            <Td isNumeric>0.91444</Td>
-          </Tr>
+          {
+            data?.data?.map(product => (
+              <Tr key={product.id}>
+                <Td>{product?.id}</Td>
+                <Td>{product?.attributes?.title}</Td>
+                <Td>{product?.attributes?.category?.data?.attributes?.title}</Td>
+                <Td>
+                  <Image 
+                    borderRadius='full'
+                    objectFit={"cover"}
+                    boxSize="40px"
+                    src={`${import.meta.env.VITE_SERVER_URL}
+                    ${product?.attributes?.thumbnail?.data?.attributes?.formats?.thumbnail?.url}`}
+                    alt={product?.attributes?.title}
+                  />
+                </Td>
+                <Td isNumeric>${product?.attributes?.price}</Td>
+                <Td isNumeric>{product?.attributes?.stock}</Td>
+                <Td>
+                  <Button 
+                    as={Link}
+                    to={`/products/${product.id}`}
+                    colorScheme='purple'
+                    variant="solid"
+                    mr={3}
+                    onClick={()=> {}}
+                  >
+                    <AiOutlineEye size={17} />
+                  </Button>
+                  <Button colorScheme='red' variant='solid' mr={3} onClick={() => {}}>
+                    <BsTrash size={17} />
+                  </Button>
+                  <Button colorScheme='blue' variant='solid' onClick={() => {}}>
+                    <FiEdit2 size={17} />
+                  </Button>
+                </Td>
+              </Tr>
+            ))
+          }
         </Tbody>
         <Tfoot>
           <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
+          <Th>ID</Th>
+            <Th>Title</Th>
+            <Th>Category</Th>
+            <Th>Thumbnail</Th>
+            <Th isNumeric>Price</Th>
+            <Th isNumeric>Stock</Th>
+            <Th>Action</Th>
           </Tr>
         </Tfoot>
       </Table>
